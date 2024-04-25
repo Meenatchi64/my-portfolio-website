2827b111e157fb90ed37435719616556cceae85e
Meenatchi64 <meenatchiv1909@gmail.com>
2024-04-25 07:02:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector('#menu-icon');
+var navbar = document.querySelector('.navbar');
+
+menu.onclick = ()=> {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open')
+};
